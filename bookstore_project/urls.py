dd34dd91b362423d80c6f93cf94cddacbbdee82c
Wences <villegaswences@gmail.com>
2020-06-06 21:44:08 -0300

    ch05

--- bookstore_project/urls.py
+++ bookstore_project/urls.py
@@ -17,6 +17,16 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    # Admin
     path('admin/', admin.site.urls),
+
+    # Accounts
+    path('accounts/', include('django.contrib.auth.urls')),
+
+
+    # Custom 
     path('', include('pages.urls')),
+    
+    path('accounts/', include('users.urls')),
+
 ]
